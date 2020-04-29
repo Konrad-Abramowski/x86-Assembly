8d6e31588dc10ec6e6359b7b8b6e6cdfd871cc60
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-04-29 14:38:26 +0200

    Test commit

---
+++ Lab3/3.1.s
@@ -0,0 +1,30 @@
+.data
+SCANF_STRING:   .ascii "%1s %4s"
+PRINTF_STRING:  .string "Char: %s ,Char*: %s"
+
+.text
+    .globl main
+    .type main, @function
+main:
+    pushl %ebp
+    movl %esp, %ebp
+    subl $8, %esp
+
+    leal 12(%ebp), %eax
+    pushl %eax
+    leal 8(%ebp), %eax
+    pushl %eax
+    pushl $SCANF_STRING
+    call scanf
+
+    leal 12(%ebp), %eax
+    pushl %eax
+    leal 8(%ebp), %eax
+    pushl %eax
+    pushl $PRINTF_STRING
+    call printf
+    addl $8, %esp
+
+
+    leave
+    ret
